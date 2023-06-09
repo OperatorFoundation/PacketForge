f4437177afe80e2d8df64320daeab9b93fdc5b4c
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2023-06-09 15:14:50 -0500

    Initial Commit

---
+++ PacketForge/PacketForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PacketForgeApp.swift
+//  PacketForge
+//
+//  Created by Dr. Brandon Wiley on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct PacketForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
